dd2265fa341298358478089812b1deb20bb340f5
Antonio Santana <antonionetosg6@gmail.com>
2024-07-31 11:49:27 -0300

    chore: version 0.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+/**
+ * Development branch @version 0.1.0
+ * Component that renders a window with general features.
+ * @returns {JSX.Element}
+ */
+export default function Home(): JSX.Element {
   return <main></main>
 }
